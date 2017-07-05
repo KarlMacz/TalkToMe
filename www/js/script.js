3a36f7afe99d112e2d3cf6bfc6304ea89e17bfa9
Karl T. Macadangdang <karljarren0308@gmail.com>
2017-07-06 00:23:15 +0800

    Added googleCustomSearchID variable and renamed googleTranslationApiKey variable to googleApiKey

--- www/js/script.js
+++ www/js/script.js
@@ -6,4 +6,5 @@ function hideLoader() {
     $('.ttmLoader').fadeOut(250);
 }
 
-var googleTranslationApiKey = 'AIzaSyCe5mqPweAAxOSjvllzxjWtN3re9Lp1a0Y';
+var googleApiKey = 'AIzaSyCe5mqPweAAxOSjvllzxjWtN3re9Lp1a0Y';
+var googleCustomSearchID = '004632487785828615457:xl5mketijie';
